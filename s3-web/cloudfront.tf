49d880efda7dbfe78546041ac032852d7f279512
khuslens <khuslen.cyber@gmail.com>
2020-03-20 15:16:47 -0500

    true is changed to false

--- s3-web/cloudfront.tf
+++ s3-web/cloudfront.tf
@@ -17,7 +17,7 @@ resource "aws_cloudfront_distribution" "demo" {
     origin_id   = local.s3_origin_id
   }
 
-  enabled             = true
+  enabled             = false
   default_root_object = "index.html"
 
   // All values are defaults from the AWS console.
