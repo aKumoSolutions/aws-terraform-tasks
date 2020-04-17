7fb141e7beecabe4dc425b8d85a11541598be88d
Khuslentuguldur <khuslens@Khuslentuguldurs-MacBook-Pro.local>
2020-04-17 14:22:56 -0500

    updated

--- s3-web/acm.tf
+++ s3-web/acm.tf
@@ -19,3 +19,5 @@ resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
+
+# https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-invalid-viewer-certificate/
